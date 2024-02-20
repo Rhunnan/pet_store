b0c0cc208d10ecb00b3abad1a13c35b0b3c4699b
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-02-20 23:16:42 +0800

    making Initial Data for UI in storeData

---
+++ lib/components/storeData.dart
@@ -0,0 +1,16 @@
+import 'store_class.dart';
+import 'package:flutter/material.dart';
+
+List<Pet> catStoreItems = [
+  Pet("Bingal Cat", "assets/images/bingalCat.png", 85.00),
+  Pet("Abyssinan Cat", "assets/images/abysCat.png", 100.00),
+  Pet("Burmese Cat", "assets/images/bingalCat.png", 60.00),
+  Pet("Persian Cat", "assets/images/persianCat.png", 99.00),
+  Pet("Russian Blue Cat", "assets/images/rusCat.png", 120.00),
+  Pet("Sphinx Cat", "assets/images/sphinxCat.png", 150.00),
+];
+
+List<Store> listOfPetStores = [
+  Store("Cat Pet Store", "assets/images/sphinxCat.png", "Colon, Cebu City",
+      catStoreItems)
+];
